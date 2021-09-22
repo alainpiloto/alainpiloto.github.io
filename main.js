e1ca41790d5f3ac771f22c38e7ed2900604c8a2f
alainpiloto <78241065+alainpiloto@users.noreply.github.com>
2021-09-22 00:01:22 -0500

    Update main.js

--- main.js
+++ main.js
@@ -16,37 +16,41 @@ function showMenu(e) {
     console.log(navContainer)
 }
 // Mansory for Mobile
-// imagesLoaded( elem, () => {
 
-    // } )
     var elem = document.querySelector(".grid-container");
 function myFunction(x) {
     if (x.matches) { // If media query matches
-        var msnry = new Masonry(elem, {
-          // options
-          itemSelector: ".grid-item",
-          columnWidth: 255,
-          gutter: 10,
-          fitWidth: true
-        });
+        imagesLoaded( elem, () => {
+            
+            var msnry = new Masonry(elem, {
+              // options
+              itemSelector: ".grid-item",
+              columnWidth: 255,
+              gutter: 10,
+              fitWidth: true
+            });
+        } )
+        
         
 
     } else {
-        
-        var msnry = new Masonry(elem, {
-          // options
-          itemSelector: ".grid-item",
-          columnWidth: 150,
-          gutter: 10,
-          fitWidth: true
-        });
+        imagesLoaded( elem, () => {
 
-        languages.remove()
+            var msnry = new Masonry(elem, {
+              // options
+              itemSelector: ".grid-item",
+              columnWidth: 150,
+              gutter: 10,
+              fitWidth: true
+            });
+    
+            languages.remove()
+        } )
     }
   }
   
   var x = window.matchMedia("(min-width: 768px)")
 //   x.addEventListener('DOMContentLoaded',myFunction(x)) // Attach listener function on state changes
-document.onload = myFunction(x)
+window.onload = myFunction(x)
 
 
