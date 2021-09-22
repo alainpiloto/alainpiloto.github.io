0f6ea0db65efef74a56f985bbe73f219f78d3294
alainpiloto <78241065+alainpiloto@users.noreply.github.com>
2021-09-21 22:33:32 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,52 @@
+const languages = document.querySelector('.languages');
+const menuIcon = document.querySelector('.icon-img');
+const navContainer = document.querySelector('.nav-links-container')
+
+menuIcon.addEventListener('click', showMenu)
+
+
+function showMenu(e) {
+    e.stopPropagation()
+    if(navContainer.classList.contains('show-menu')) {
+        navContainer.classList.remove('show-menu')
+    } else {
+
+        navContainer.classList.add('show-menu')
+    }
+    
+    console.log(navContainer)
+}
+// Mansory for Mobile
+
+
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+        var elem = document.querySelector(".grid-container");
+        var msnry = new Masonry(elem, {
+          // options
+          itemSelector: ".grid-item",
+          columnWidth: 255,
+          gutter: 10,
+          fitWidth: true
+        });
+        
+
+    } else {
+        var elem = document.querySelector(".grid-container");
+        var msnry = new Masonry(elem, {
+          // options
+          itemSelector: ".grid-item",
+          columnWidth: 150,
+          gutter: 10,
+          fitWidth: true
+        });
+
+        languages.remove()
+    }
+  }
+  
+  var x = window.matchMedia("(min-width: 768px)")
+  myFunction(x) // Call listener function at run time
+  x.addEventListener('DOMContentLoaded',myFunction) // Attach listener function on state changes
+
+
