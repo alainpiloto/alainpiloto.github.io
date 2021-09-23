2d20a6438b4648cc781bb576cebd39e7787fd399
alainpiloto <78241065+alainpiloto@users.noreply.github.com>
2021-09-22 20:23:10 -0500

    adding more animations

--- main.js
+++ main.js
@@ -5,6 +5,27 @@ var x = window.matchMedia("(min-width: 768px)")
 var elem = document.querySelector(".grid-container");
 const emailImg = document.querySelector('.email-img')
 const emailText = document.querySelector('.email-text')
+const mailFooter = document.querySelector('.mail-icon-footer');
+const jsDriver = document.querySelector('.jsDriver')
+const closeIcon = document.querySelector('.closeIcon');
+
+
+mailFooter.addEventListener('click', showPopUpMail)
+
+function showPopUpMail(e) {
+    const mailMobileContainer = document.querySelector('.mailMobileContainer')
+    if(jsDriver.classList.contains('hide-email')){
+        jsDriver.classList.remove('hide-email')
+        mailMobileContainer.classList.add('animate__jackInTheBox')
+        console.log('opening mail')
+
+    }
+}
+
+closeIcon.addEventListener('click', () => {
+    jsDriver.classList.add('hide-email')
+})
+                    /*******Email Animation ********/
 
 setInterval(() => {
     if(emailText.classList.contains('showEmail')){
@@ -16,15 +37,11 @@ setInterval(() => {
     }, 1000);
 }, 3000);
 
-
+                    /************ TOP NAV Responsive*************/
 menuIcon.addEventListener('click', showMenu)
 
 navContainer.addEventListener('click', hideNavBar)
 
-window.addEventListener('scroll', () => {
-    const header = document.querySelector('.header-home');
-    header.classList.toggle('down', window.scrollY > 20)
-} )
 
 emailImg.addEventListener('click', () => {
     console.log('click en email')
@@ -40,8 +57,12 @@ emailImg.addEventListener('click', () => {
 function hideNavBar(e) {
     if(e.target.classList.contains('hideNav')) {
         navContainer.classList.remove('show-menu')
-            }
+    }
 }
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('.header-home');
+    header.classList.toggle('down', window.scrollY > 20)
+} )
 
 window.addEventListener('DOMContentLoaded', () => {
     if(!x.matches) {
@@ -50,18 +71,18 @@ window.addEventListener('DOMContentLoaded', () => {
     }   
     
 })
-//   x.addEventListener('DOMContentLoaded',myFunction(x)) // Attach listener function on state changes
-window.onload = changeColumnWidth(x)
+
 
 function showMenu(e) {
     if(navContainer.classList.contains('show-menu')) {
         navContainer.classList.remove('show-menu')
     } else {
-
+        
         navContainer.classList.add('show-menu')
     }   
 }
 // Mansory for Mobile
+window.onload = changeColumnWidth(x)
 
 function changeColumnWidth(x) {
     if (x.matches) { // If media query matches
